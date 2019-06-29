48c0de89057eca9ce66ee6ca57e8616feb64bbb6
Peter Sampson <petersampson010@gmail.com>
2019-06-29 14:19:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if x / 3 == 0
-    puts "Fizz"
+  if int % 3 == 0
+    "Fizz"
   end
 end 
 
